20abe6667c156a472fc3e304e4ad0cea42a8096b
Li Hao <lihao@nclouds.com>
2015-05-12 15:35:43 +0800

    test

--- cookbooks/ops-test/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ops-test'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures ops-test'
-long_description 'Installs/Configures ops-test'
-version          '0.1.0'
