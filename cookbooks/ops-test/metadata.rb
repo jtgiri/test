fa5db1d24d63d4e984cfa8a93a7c25217572cf6f
Li Hao <lihao@nclouds.com>
2015-05-12 14:37:50 +0800

    Add test cookbook

---
+++ cookbooks/ops-test/metadata.rb
@@ -0,0 +1,7 @@
+name             'ops-test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ops-test'
+long_description 'Installs/Configures ops-test'
+version          '0.1.0'
