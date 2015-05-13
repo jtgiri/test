65faca238d807c2d889fa009fae308013b07ff7d
Li Hao <lihao@nclouds.com>
2015-05-13 15:26:21 +0800

    test

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,5 +2,7 @@
 echo "start" >> /var/www/deploy.txt
 date  >> /var/www/deploy.txt
 if [ -f /var/www/app/config.txt ]; then
-   cp /var/www/app/config.txt  /var/www/config.txt
+   cp /var/www/app/config.txt  /var/www/app-bak/config.txt
 fi
+mkdir -p /var/www/app-bak
+mv /var/www/app /var/www/app-bak/app-`date +%Y%m%d%H%M%S`
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "restore" >> /var/www/deploy.txt
 date >> /var/www/deploy.txt
-if [ -f /var/www/app/config.txt ]; then
-   cp -f /var/www/config.txt /var/www/app/config.txt
+if [ -f /var/www/app-bak/config.txt ]; then
+   cp -f /var/www/app-bak/config.txt /var/www/app/config.txt
 fi
