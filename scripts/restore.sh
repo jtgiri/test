c29f81e43d331d226167f4344bce1b0cb4bce12d
Li Hao <lihao@nclouds.com>
2015-05-13 15:14:37 +0800

    test

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "restore" >> /var/www/deploy.txt
 date >> /var/www/deploy.txt
-cp -f /var/www/config.txt /var/www/app/config.txt  
+if [ -f /var/www/app/config.txt ]; then
+   cp -f /var/www/config.txt /var/www/app/config.txt
+fi
